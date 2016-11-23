87c751dc5d5d2d8aea96bbdf687d477aafffb5a8
kavitaji <kavitha.ashok2305@gmail.com>
2016-11-23 08:41:55 +0000

    6th nano project xyz reader

---
+++ xyzreader/XYZReader/src/main/java/com/example/xyzreader/ui/BaseActivity.java
@@ -0,0 +1,10 @@
+package com.example.xyzreader.ui;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ashwath on 11/8/2016.
+ */
+
+public class BaseActivity extends AppCompatActivity{
+}
